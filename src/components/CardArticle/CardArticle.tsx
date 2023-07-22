c34262c55f616fff31552c057b61f3fb61e9f8a4
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-07-22 18:29:23 -0300

    FEAT[CardArtciles]: Adição da forma dinamica de exibição dos artigos

--- src/components/CardArticle/CardArticle.tsx
+++ src/components/CardArticle/CardArticle.tsx
@@ -1,20 +1,60 @@
+interface ObjetoArticle {
+  id: number;
+  imgSrc: string;
+  imgAlt: string;
+  tilte: string;
+  description: string;
+  linkSrc: string;
+}
+
+const arrayArticles: ObjetoArticle[] = [
+  { 
+    id: 1,
+    imgSrc: "/articles/nutricao-esportiva.jpg", 
+    imgAlt: "Imagem Nutrição Esportiva",
+    title: "Nutrição Esportiva",
+    description: "Aplicação dos conhecimentos em nutrição, bioquímica e fisiologia na área do esporte, a fim de manter o equilíbrio das necessidades energéticas, através da indicação de nutrientes necessários a cada organismo, conforme a modalidade.",
+    linkSrc: "https://www.ipgs.com.br/saiba-tudo-sobre-a-nutricao-esportiva-e-os-seus-segmentos/"
+  },
+  {
+    id: 2,
+    imgSrc: "/articles/intolerancias-alimentares.jpg", 
+    imgAlt: "Imagem Intolerâncias Alimentares",
+    title: "Intolerâncias Alimentares",
+    description: "Existem diversos tipos de intolerância e alergia alimentar. A nutrição tem como objetivo analisar quais alimentos causam alergia ou intolerância ao paciente e orientá-lo, indicando uma dieta adequada e individualizada.",
+    linkSrc: "https://www.tuasaude.com/sintomas-de-intolerancia-alimentar/"
+  },
+  {
+    id: 3,
+    imgSrc: "/articles/nutricao-ortomolecular.jpg", 
+    imgAlt: "Imagem Nutrição Ortomolecular",
+    title: "Nutrição Ortomolecular",
+    description: "Tem como principal objetivo promover o equilíbrio molecular do organismo, promovendo saúde através da análise do excesso ou da falta de nutrientes.",
+    linkSrc: "https://sophiederam.com/br/nutricao-ortomolecular/"
+  },
+];
+
 export default function CardArticle(){
   return (
       <>
-        <div className="w-auto mb-10 rounded">
-          <img className="w-full rounded-tl rounded-tr" src="/articles/nutricao-esportiva.jpg" alt="Imagem do Artigo" />
+        {arrayArticles.map((article) => (
+          <div key={article.id} className="w-auto max-w-2xl mb-10 rounded">
+          <img className="w-full rounded-tl rounded-tr" src={article.imgSrc} alt={article.imgAlt} />
           <div className="w-full p-3 bg-white rounded-bl rounded-br">
-            <h2 className="text-2xl text-lime-600">Nutrição Esportiva</h2>
+            <h2 className="text-2xl text-lime-600">{article.title}</h2>
             <p>
-            Nutrição EsportivaAplicação dos conhecimentos em nutrição, bioquímica e fisiologia na área do esporte, a fim de manter o equilíbrio das necessidades energéticas, através da indicação de nutrientes necessários a cada organismo, conforme a modalidade.
+            {article.description}
             </p>
-            <a className="decoration-0" href="https://www.ipgs.com.br/saiba-tudo-sobre-a-nutricao-esportiva-e-os-seus-segmentos/" target="_blank" >
+            <a className="decoration-0" href={article.linkSrc} target="_blank" >
             <button className="rounded mt-8 px-3 py-2 text-center font-bold bg-lime-500 text-white transition-all duration-300 hover:bg-lime-600 hover:transition-all hover:duration-300">
               Leia mais
             </button>
             </a>
           </div>
         </div>
+        ))}
+        
+        
       </>
     )
 }
